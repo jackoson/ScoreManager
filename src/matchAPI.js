44d72f2246ab0267f87fac57d1d3d194a29ab17a
Samuel Russell <sr14978@bristol.ac.uk>
2017-03-24 11:43:18 +0000

    changed addmatch to post

--- src/matchAPI.js
+++ src/matchAPI.js
@@ -11,9 +11,8 @@ router.get('/id/:id', function (req, res) {
   matchesController.getMatchByID(req.params.id, function(err,data){ res.send(data); })
 })
 
-//http://localhost:8080/matches/add?params={"type":"mens singles","datetime":"2000-01-01 00:00:00","players":[{"id":1,"setsWon":2},{"id":4,"setsWon":1}]}
-router.get('/add', function (req, res) {
-  var details = JSON.parse(req.query.params);
+router.post('/add', function (req, res) {
+  var details = JSON.parse(req.body.params);
   matchesController.addMatch(details.players, details.type, details.datetime, function(err){ if (err == null) {res.send("success");} else {res.send("fail");}; })
 })
 
--- src/server.js
+++ src/server.js
@@ -5,6 +5,10 @@ var match_api = require("./matchAPI");
 var express = require('express');
 var app = express();
 
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(express.static("public"));
 app.use('/players', player_api);
 app.use('/matches', match_api);
