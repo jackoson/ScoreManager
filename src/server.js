fadb0519f25fb86c51763ab1a48bec2d6772cead
Samuel Russell <sr14978@bristol.ac.uk>
2017-04-01 18:25:54 +0100

    tidied server and created responsive splash screen

--- src/server.js
+++ src/server.js
@@ -7,23 +7,32 @@ var app = express();
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(function (error, req, res, next){
-    if (error.message != undefined && error.message.includes("Unexpected token")){
-        res.send("Invalid json");
-    }else{
-        next ();
-    }
-})
+app.use(handleBodyParseError)
 
-app.use(express.static("public"));
+
+var options = { setHeaders: deliverXHTML };
+app.use(express.static("public", options));
 app.use('/players', APIs.players);
 app.use('/matches', APIs.matches);
 app.use('/competitions', APIs.competitions);
 app.use('/teams', APIs.teams);
 
 var port = 8080;
-var address = "localhost"
+var address = "192.168.0.12"//"localhost"
 app.listen(port, address);
 
 console.log("visit...");
 console.log(address+":"+port);
+
+
+function deliverXHTML(res, path, stat) {
+    if (path.endsWith(".html"))
+        res.header("Content-Type", "application/xhtml+xml");
+}
+
+function handleBodyParseError(error, req, res, next){
+    if (error.message != undefined && error.message.includes("Unexpected token"))
+        res.send("Invalid json");
+    else
+        next ();
+}
