303cb5a559ee617baa877dff85a74e743f9a37fe
Samuel Russell <sr14978@bristol.ac.uk>
2017-05-21 01:41:06 +0100

    added cookies - landing only shows first time

--- src/server.js
+++ src/server.js
@@ -1,8 +1,6 @@
 "use strict"
 var fs = require("fs");
 var path = require("path");
-var APIs = require("./DataAPIs/APIController");
-var templateManager = require('./TemplateManager');
 var express = require('express');
 var https = require('https');
 var http = require('http');
@@ -13,7 +11,23 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(handleBodyParseError)
 
+var cookieParser = require('cookie-parser')
+app.use(cookieParser())
+
 app.use(log_request);
+
+var APIs = require("./DataAPIs/APIController");
+var templateManager = require('./TemplateManager');
+
+app.get('/', function(req, res) {
+  if(req.cookies.visited != null && req.cookies.visited == "true") {
+    res.cookie("visited", "true", {expires: new Date(new Date().setFullYear(new Date().getFullYear() + 10))});
+    res.redirect("/templates/home/");
+  } else {
+    res.cookie("visited", "true", {expires: new Date(new Date().setFullYear(new Date().getFullYear() + 10))});
+    res.sendFile(path.resolve(__dirname, "public/landing.html"));
+  }
+})
 
 var options = { setHeaders: deliverXHTML_static, index: "landing.html" };
 app.use(express.static(path.resolve(__dirname, 'public'), options));
