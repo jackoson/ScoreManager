7d7a5e2ab7bd2fc12c540487e585944060a6373b
Samuel Russell <sr14978@bristol.ac.uk>
2017-05-21 01:42:44 +0100

    added modal to all templatess

--- src/TemplateManager.js
+++ src/TemplateManager.js
@@ -8,35 +8,35 @@ var api = require('./DataAPIs/DatabaseProviders/DataProviderController');
 router.get('/players/id/:id', function (req, res) {
   api.players.getByID(req.params.id, (err, data) => {
     if( err != null ) {res.send(err);}
-    res.render('player', {player: data});
+    res.render('player', {player: data, logged_in: true});
   })
 })
 
 router.get('/players', function (req, res) {
   api.players.getAll((err, data) => {
     if( err != null ) {res.send(err);}
-    res.render('players', {players: data});
+    res.render('players', {players: data, logged_in: true});
   })
 })
 
 router.get('/matches', function (req, res) {
   api.matches.getAll((err, data) => {
     if( err != null ) {res.send(err);}
-    res.render('matches', {matches: data});
+    res.render('matches', {matches: data, logged_in: true});
   })
 })
 
 router.get('/teams', function (req, res) {
   api.teams.getAll((err, data) => {
     if( err != null ) {res.send(err);}
-    res.render('teams', {teams: data});
+    res.render('teams', {teams: data, logged_in: true});
   })
 })
 
 router.get('/competitions', function (req, res) {
   api.competitions.getAll((err, data) => {
     if( err != null ) {res.send(err);}
-    res.render('competitions', {competitions: data});
+    res.render('competitions', {competitions: data, logged_in: true});
   })
 })
 
